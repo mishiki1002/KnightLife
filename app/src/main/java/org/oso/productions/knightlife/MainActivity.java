4440d23951cc22499561af15c7777d41421a391e
mishiki1002 <brownbear1002@gmail.com>
2017-12-29 09:37:34 -0500

    Added icons / Added menu functionality and removed image header

--- app/src/main/java/org/oso/productions/knightlife/MainActivity.java
+++ app/src/main/java/org/oso/productions/knightlife/MainActivity.java
@@ -13,12 +13,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     TextView content;
     String TAG;
+
+    WebView website;
+    String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +42,45 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        url = "https://knightlifenews.com/";
+        website = (WebView) findViewById(R.id.main_webview);
+
+        WebSettings webSettings = website.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        website.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url)
+            {
+                Log.d(TAG, "Removing Navigation 1 REPLACE");
+                view.loadUrl("javascript:(function() { " +
+                        "var head = document.getElementsByTagName('header')[0];"
+                        + "head.parentNode.removeChild(head);" +
+                        "})()");
+                Log.d(TAG, "Removing Search 2 REPLACE");
+                view.loadUrl("javascript:(function() { " +
+                        "var head = document.getElementsByTagName('form')[0];"
+                        + "head.parentNode.removeChild(head);" +
+                        "})()");
+                Log.d(TAG, "Removing Naviagtion Menu 3 REPLACE");
+                view.loadUrl("javascript:(function() { " +
+                        "var head = document.getElementById('menu-catergories-container');"
+                        + "head.parentNode.removeChild(head);" +
+                        "})()");
+
+            }
+        });
+
         content = (TextView) findViewById(R.id.main_content);
         TAG = "MAIN";
+
+        website.loadUrl(url);
     }
 
     @Override
@@ -81,33 +124,53 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if (id == R.id.nav_home) {
             Log.d(TAG, "Nav Home Selelcted");
             content.setText("Home");
+            url = "https://knightlifenews.com/";
+            website.loadUrl(url);
         } else if (id == R.id.nav_feature) {
             Log.d(TAG, "Nav Feature Selected");
             content.setText("Feature");
+            url = "https://knightlifenews.com/category/feature/";
+            website.loadUrl(url);
         } else if (id == R.id.nav_opinion) {
             Log.d(TAG, "Nav Opinion Selected");
             content.setText("Opinion");
+            url = "https://knightlifenews.com/category/opinion/";
+            website.loadUrl(url);
         } else if (id == R.id.nav_news) {
             Log.d(TAG, "Nav News Selected");
             content.setText("News");
+            url = "https://knightlifenews.com/category/news/";
+            website.loadUrl(url);
         } else if (id == R.id.nav_sports) {
             Log.d(TAG, "Nav Sports Selelcted");
             content.setText("Sports");
+            url = "https://knightlifenews.com/category/sports/";
+            website.loadUrl(url);
         } else if (id == R.id.nav_AE) {
             Log.d(TAG, "Nav AE Selected");
             content.setText("A&E");
+            url = "https://knightlifenews.com/category/ae/";
+            website.loadUrl(url);
         } else if (id == R.id.nav_uncatagorized) {
             Log.d(TAG, "Nav Uncatagorized Selected");
             content.setText("Uncatagorized");
+            url = "https://knightlifenews.com/category/uncategorized/";
+            website.loadUrl(url);
         } else if (id == R.id.nav_comics) {
             Log.d(TAG, "Nav Comics Selected");
             content.setText("Comics");
+            url = "https://knightlifenews.com/category/comics/";
+            website.loadUrl(url);
         } else if (id == R.id.nav_videos) {
             Log.d(TAG, "Nav Videos Selected");
             content.setText("Videos");
+            url = "https://knightlifenews.com/category/videos/";
+            website.loadUrl(url);
         } else if (id == R.id.nav_poll) {
             Log.d(TAG, "Nav Poll Selected");
             content.setText("Polls");
+            url = "https://knightlifenews.com/category/poll/";
+            website.loadUrl(url);
         } else if (id == R.id.nav_share) {
             Log.d(TAG, "Nav Share Selected");
             content.setText("Share");
